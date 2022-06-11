f112f886ec27e0e8614ac0b4a5400299991675e7
AndersBulon <home-bit@mail.ru>
2022-06-11 13:45:40 +0300

    Презентационная компонента Users очищена от ajax запросов. Для этого создана компонентаUsersApiContauner

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,98 +1,38 @@
-import axios from "axios";
 import React from "react";
 import style from "./Users.module.css"
 import image from "../../assist/images/user.jpg"
 
-class Users extends React.Component {
-
-	// constructor (props) {
-	// 	super(props);
-	// }
-
-	componentDidMount() {
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-			this.props.setUsers(response.data.items)
-			this.props.setTotalUsersCount(response.data.totalCount)
-		})
-	}
-	numPageChanged = (el) => {
-		this.props.setCurrentPage(el)
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${el}&count=${this.props.pageSize}`).then(response => {
-			this.props.setUsers(response.data.items)
-		})
-	}
-	showMore = () => {
-		if (this.props.currentPagesBlock < this.props.totalBlockCount) {
-			let currBlock = this.props.currentPagesBlock;
-			currBlock++;
-			this.props.showMore(currBlock)
-			axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currBlock}&count=${this.props.pageSize}`).then(response => {
-				this.props.setUsers(response.data.items)
-			})
-		}
-	}
-	showPrevBlock = () => {
-		if (this.props.currentPagesBlock > 1) {
-			let currBlock = this.props.currentPagesBlock;
-			currBlock--;
-			this.props.showPrevBlock(currBlock)
-			axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currBlock}&count=${this.props.pageSize}`).then(response => {
-				this.props.setUsers(response.data.items)
-			})
-		}
-	}
-	goEndPageNumber = () => {
-		if (this.props.currentPagesBlock < this.props.totalBlockCount) {
-			this.props.goEndPageNumber()
-			axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.pages}&count=${this.props.pageSize}`).then(response => {
-				this.props.setUsers(response.data.items)
-			})
-		}
-	}
-	goFirstPageNumber = () => {
-		if (this.props.currentPagesBlock > 1) {
-			this.props.goFirstPageNumber()
-			setTimeout(() => {
-				axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-				this.props.setUsers(response.data.items)
-			})
-			}, 50);
-			
-		}
-	}
-
-	render() {
-
-
-		return (
+let  Users=  (props) => {
+	return (
 
 			<div className={`${style.content} designe`} >
-				<h4>Блок страниц : {this.props.currentPagesBlock} *** Cтраничка : {this.props.currentPage} </h4>
+				<h4>Страничка пользователей</h4>
 
 				<div className={style.pages}>
+					<span onClick={() => { props.goFirstPageNumber() }} className={props.currentPagesBlock === 1 ? style.nextpageblock : style.nextpageblock_activ}>
+						&lt;&lt;
+					</span>
 
-					<span onClick={() => {this.goFirstPageNumber()}} className={this.props.currentPagesBlock === 1 ? style.nextpageblock : style.nextpageblock_activ}>&lt;&lt;</span>
-
-
-					{this.props.blockStructure[this.props.currentPagesBlock].map(el =>
-
-						<span onClick={() => this.numPageChanged(el)} key={el} className={this.props.currentPage === el ?
-							style.selectedPage : style.pageNum}>{el}</span>
+					{props.blockStructure[props.currentPagesBlock].map(el =>
+						<span onClick={() => props.numPageChanged(el)} key={el} className={props.currentPage === el ? style.selectedPage : style.pageNum}>
+							{el}
+						</span>
 					)}
-					<span onClick={() => { this.goEndPageNumber() }} className={this.props.currentPagesBlock ===
-						this.props.totalBlockCount ? style.prevpageblock : style.prevpageblock_activ}>&gt;&gt;</span>
+
+					<span onClick={() => { props.goEndPageNumber() }} className={props.currentPagesBlock === props.totalBlockCount ? style.prevpageblock : style.prevpageblock_activ}>
+						&gt;&gt;
+					</span>
 				</div>
 
-
 				<div className={style.item}>
-					{this.props.users.map(user =>
+					{props.users.map(user =>
 						<div className={style.userswrapper} key={user.id}>
 							<div className={style.avatar}>
 								<img src={user.photos.small ? user.photos.small : image} alt="avatar" />
 							</div>
 							{user.followed ?
-								<button onClick={() => { this.props.follow(user.id) }} className={`${style.followBtn} button`}>Follow</button> :
-								<button onClick={() => { this.props.unfollow(user.id) }} className={`${style.followBtn} button`}>Unfollow</button>}
+								<button onClick={() => { props.follow(user.id) }} className={`${style.followBtn} button`}>Follow</button> :
+								<button onClick={() => { props.unfollow(user.id) }} className={`${style.followBtn} button`}>Unfollow</button>}
 							<div className={user.followed ? style.info_act : style.info_disact}>
 								<div className={style.status}>{user.status ? user.status.slice(0, 30) : "Нет статуса"}</div>
 								<div className={style.fullname}>{user.name}</div>
@@ -101,15 +41,13 @@ class Users extends React.Component {
 							</div>
 						</div>)}
 				</div>
+
 				<div className={style.buttons_block}>
-					<button onClick={() => { this.showPrevBlock() }} className={`${style.showbtn} button`}>PREVIOUS</button>
-					<button onClick={() => { this.showMore() }} className={`${style.showbtn} button`}>NEXT</button>
+					<button onClick={() => { props.showPrevBlock() }} className={`${style.showbtn} button`}>PREVIOUS</button>
+					<button onClick={() => { props.showNextBlock() }} className={`${style.showbtn} button`}>NEXT</button>
 				</div>
-
 			</div>
-		)
-	}
-
+	)
 }
 
 export default Users;
--- src/pages/Users/UsersContainer.jsx
+++ src/pages/Users/UsersContainer.jsx
@@ -1,59 +1,116 @@
+import axios from "axios";
+import React from "react";
 import { connect } from "react-redux";
 import { follow_AC, goEndPageNumber_AC, goFirstPageNumber_AC, setCurrentPage_AC, setTotalUsersCount_AC, setUsers_AC, showMore_AC, showPrevBlock_AC, unfollow_AC } from "../../redux/users_reducer.js";
 import Users from "./Users.jsx";
 
 
 
-let mapDispatchToProps = (dispatch) => {
-	return {
-		follow: (userId) => {
-			dispatch(follow_AC(userId))
-		},
-		unfollow: (userId) => {
-			dispatch(unfollow_AC(userId))
-		},
-		setUsers: (users) => {
-			dispatch(setUsers_AC(users))
-		},
-		setCurrentPage: (currentPage) => {
-			dispatch(setCurrentPage_AC(currentPage))
-		},
-		setTotalUsersCount: (TotalUsersCount) => {
-			dispatch(setTotalUsersCount_AC(TotalUsersCount))
-		},
-		showMore: (currBlock) => {
-			dispatch(showMore_AC(currBlock))
-		},
-		showPrevBlock: (currBlock) => {
-			dispatch(showPrevBlock_AC(currBlock))
-		},
+class UsersApiContainer extends React.Component {
+	// constructor (props) {
+	// 	super(props);
+	// }
 
-		goEndPageNumber: () => {
-			dispatch(goEndPageNumber_AC())
-		},
-		goFirstPageNumber: () => {
-			dispatch(goFirstPageNumber_AC())
-		},
-
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+			this.props.setUsers(response.data.items)
+			this.props.setTotalUsersCount(response.data.totalCount)
+		})
+	}
+	numPageChanged = (el) => {
+		this.props.setCurrentPage(el)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${el}&count=${this.props.pageSize}`).then(response => {
+			this.props.setUsers(response.data.items)
+		})
+	}
+	showNextBlock = () => {
+		if (this.props.currentPagesBlock < this.props.totalBlockCount) {
+			let currBlock = this.props.currentPagesBlock;
+			currBlock++;
+			this.props.showMore(currBlock)
+			axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currBlock}&count=${this.props.pageSize}`).then(response => {
+				this.props.setUsers(response.data.items)
+			})
+		}
+	}
+	showPrevBlock = () => {
+		if (this.props.currentPagesBlock > 1) {
+			let currBlock = this.props.currentPagesBlock;
+			currBlock--;
+			this.props.showPrevBlock(currBlock)
+			axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currBlock}&count=${this.props.pageSize}`).then(response => {
+				this.props.setUsers(response.data.items)
+			})
+		}
+	}
+	goEndPageNumber = () => {
+		if (this.props.currentPagesBlock < this.props.totalBlockCount) {
+			this.props.goEndPageNumber()
+			axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.pages}&count=${this.props.pageSize}`).then(response => {
+				this.props.setUsers(response.data.items)
+			})
+		}
+	}
+	goFirstPageNumber = () => {
+		if (this.props.currentPagesBlock > 1) {
+			this.props.goFirstPageNumber()
+			setTimeout(() => {
+				axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+					this.props.setUsers(response.data.items)
+				})
+			}, 50);
 		}
 	}
 
+	render() {
+		return (
+			<Users 
+				goFirstPageNumber={this.goFirstPageNumber}
+				numPageChanged={this.numPageChanged}
+				goEndPageNumber={this.goEndPageNumber}
+				showPrevBlock={this.showPrevBlock}
 
-let mapStateToProps = (state) => {
-	return {
-		users: state.usersPage.users,
-		pageSize: state.usersPage.pageSize,
-		totalUsersCount: state.usersPage.totalUsersCount,
-		currentPage: state.usersPage.currentPage,
-		currentPagesBlock: state.usersPage.currentPagesBlock,
-		blockStructure: state.usersPage.blockStructure,
-		totalBlockCount: state.usersPage.totalBlockCount,
-		pages: state.usersPage.pages
+				currentPagesBlock={this.props.currentPagesBlock}
+				blockStructure={this.props.blockStructure}
+				currentPage={this.props.currentPage}
+				totalBlockCount={this.props.totalBlockCount}
+				users={this.props.users}
+				
+				follow={this.props.follow}
+				unfollow={this.props.unfollow}
+			/>
+		)
 	}
-
 }
 
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+let mapDispatchToProps = (dispatch) => {
+	return {
+		follow: (userId) => { dispatch(follow_AC(userId)) },
+		unfollow: (userId) => { dispatch(unfollow_AC(userId)) },	
+		setUsers: (users) => { dispatch(setUsers_AC(users)) },
+		setCurrentPage: (currentPage) => { dispatch(setCurrentPage_AC(currentPage)) },
+		setTotalUsersCount: (TotalUsersCount) => { dispatch(setTotalUsersCount_AC(TotalUsersCount)) },
+		showMore: (currBlock) => { dispatch(showMore_AC(currBlock)) },
+		showPrevBlock: (currBlock) => { dispatch(showPrevBlock_AC(currBlock)) },
+		goEndPageNumber: () => { dispatch(goEndPageNumber_AC()) },
+		goFirstPageNumber: () => { dispatch(goFirstPageNumber_AC()) },
+	}
+}
+
+let mapStateToProps = (state) => {
+	return {
+		currentPagesBlock: state.usersPage.currentPagesBlock,
+		blockStructure: state.usersPage.blockStructure,
+		currentPage: state.usersPage.currentPage,
+		totalBlockCount: state.usersPage.totalBlockCount,
+		users: state.usersPage.users,
+		pageSize: state.usersPage.pageSize,
+		totalUsersCount: state.usersPage.totalUsersCount,
+		pages: state.usersPage.pages
+	}
+}
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersApiContainer)
 
 export default UsersContainer;
